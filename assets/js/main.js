2d3bcf0e824a4cbaa3ec8c34e6ee827a209f7777
Harshvardhan Kadam <harshvardhankadam89@gmail.com>
2022-11-23 00:09:26 +0530

    theme changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,12 +135,9 @@ const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = darkTheme
+const selectedIcon = iconTheme
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'light' : 'dark'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
@@ -154,7 +151,4 @@ themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
 })
