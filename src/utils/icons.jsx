f1d58cf1c38d63c31cce23a4b8174c736a0cc336
TheHarshKadam <harshvardhankadam89@gmail.com>
2026-04-07 00:03:26 +0530

    added common utils

---
+++ src/utils/icons.jsx
@@ -0,0 +1,36 @@
+import {
+  FaReact, FaNodeJs, FaPython, FaDocker, FaAws, FaGitAlt, FaHtml5, FaCss3Alt
+} from "react-icons/fa";
+
+import {
+  SiJavascript, SiMongodb, SiExpress, SiKubernetes, SiTailwindcss,
+  SiRedux, SiPostman, SiJira, SiGithub, SiGitlab, SiThreedotjs, SiAxios,
+  SiFlutter, SiDart, SiSolidity
+} from "react-icons/si";
+
+export const skillIcons = {
+  "React": <FaReact color="#61DBFB" />,
+  "React.js": <FaReact color="#61DBFB" />,
+  "Three.js": <SiThreedotjs color="#fff" />,
+  "Axios": <SiAxios color="#5A29E4" />,
+  "Node.js": <FaNodeJs color="#3C873A" />,
+  "Express.js": <SiExpress color="#fff" />,
+  "Mongo DB": <SiMongodb color="#47A248" />,
+  "MongoDB": <SiMongodb color="#47A248" />,
+  "Tailwind CSS": <SiTailwindcss color="#38BDF8" />,
+  "Javascript": <SiJavascript color="#F7DF1E" />,
+  "REST APIs": "🔗",
+  "Custom Middleware": "🧩",
+  "Lucide": "✨",
+
+  "Solidity": <SiSolidity color="#363636" />,
+  "Web3.js": "🌐",
+  "IPFS": "📦",
+  "MetaMask": "🦊",
+
+  "Flutter": <SiFlutter color="#02569B" />,
+  "Dart": <SiDart color="#0175C2" />,
+  "RESTAPIs": "🔗",
+  "Andriod Studio": "🤖",
+  "iOS": "🍎",
+};
